c8b611368e567cd965584067f26e31b41f87bda7
Serge018 <sivenskij@gmail.com>
2024-01-20 16:46:17 +0300

    Внесена структура проекта

---
+++ src/main/java/org/githab/Serge018/GeekBrainsCoursTestsApi/App.java
@@ -0,0 +1,13 @@
+package org.githab.Serge018.GeekBrainsCoursTestsApi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/githab/Serge018/GeekBrainsCoursTestsApi/AppTest.java
@@ -0,0 +1,20 @@
+package org.githab.Serge018.GeekBrainsCoursTestsApi;
+
+import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        Assertions.assertTrue( true );
+    }
+}

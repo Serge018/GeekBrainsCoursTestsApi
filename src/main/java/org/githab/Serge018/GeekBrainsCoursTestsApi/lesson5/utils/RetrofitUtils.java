54e0e7f710c7abcb29bf77079b617e966e3d1c67
Serge018 <sivenskij@gmail.com>
2024-01-23 19:09:37 +0300

    Добавлено логироание с использованием библиотеки okhttp3

--- src/main/java/org/githab/Serge018/GeekBrainsCoursTestsApi/lesson5/utils/RetrofitUtils.java
+++ src/main/java/org/githab/Serge018/GeekBrainsCoursTestsApi/lesson5/utils/RetrofitUtils.java
@@ -1,19 +1,33 @@
 package org.githab.Serge018.GeekBrainsCoursTestsApi.lesson5.utils;
 
 import lombok.experimental.UtilityClass;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 import java.io.IOException;
 
 
+import static okhttp3.logging.HttpLoggingInterceptor.Level.BODY;
+
 @UtilityClass
 public class RetrofitUtils
 {
+    OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+    HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+    // Собственный перехвадчик
+    // LoggingInterceptor logging = new LoggingInterceptor();
+
     public Retrofit getRetrofit() throws IOException
     {
+        // Настраиваем перехватчик для http клиента
+        logging.setLevel(BODY);
+        httpClient.addInterceptor(logging);
+
         Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(ConfigUtils.getBaseUrl())
             .addConverterFactory(JacksonConverterFactory.create())
+            .client(httpClient.build())
             .build();
 
         return retrofit;

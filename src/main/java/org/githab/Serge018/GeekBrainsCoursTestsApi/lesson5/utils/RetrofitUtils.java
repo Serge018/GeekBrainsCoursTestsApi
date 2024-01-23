711e82dc2fa5d10881a7f5f5ace2d512cfe50cad
Serge018 <sivenskij@gmail.com>
2024-01-23 18:35:21 +0300

    Удалён закомментриованный код

--- src/main/java/org/githab/Serge018/GeekBrainsCoursTestsApi/lesson5/utils/RetrofitUtils.java
+++ src/main/java/org/githab/Serge018/GeekBrainsCoursTestsApi/lesson5/utils/RetrofitUtils.java
@@ -3,19 +3,12 @@ package org.githab.Serge018.GeekBrainsCoursTestsApi.lesson5.utils;
 import lombok.experimental.UtilityClass;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
-//import okhttp3.logging.HttpLoggingInterceptor;
-//import okhttp3.OkHttpClient;
-//import static okhttp3.logging.HttpLoggingInterceptor.Level.BODY;
 import java.io.IOException;
 
 
 @UtilityClass
 public class RetrofitUtils
 {
-//    HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
-//    LoggingInterceptor logging2 = new LoggingInterceptor();
-//    OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-
     public Retrofit getRetrofit() throws IOException
     {
         Retrofit retrofit = new Retrofit.Builder()
